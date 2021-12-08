104920a2228d32b1e3eccd9ebb307a07cc5a7a0a
Ditta <ditta.khairunnisa@cartenz.co.id>
2021-12-08 08:51:16 +0700

    bug fix nodes

--- app-streetboxuser/src/main/java/id/streetbox/live/ui/main/doortodoor/notification/NotificationDoortoDoorMapsActivity.kt
+++ app-streetboxuser/src/main/java/id/streetbox/live/ui/main/doortodoor/notification/NotificationDoortoDoorMapsActivity.kt
@@ -9,7 +9,9 @@ import android.view.animation.LinearInterpolator
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.*
+import com.zeepos.map.utils.AnimationUtils
 import com.zeepos.models.ConstVar
+import com.zeepos.models.entities.MapData
 import com.zeepos.models.response.DataFoodTruck
 import com.zeepos.models.response.DataItemGetStatusCall
 import com.zeepos.models.response.DataItemNotificationBlast
@@ -44,6 +46,10 @@ class NotificationDoortoDoorMapsActivity :
     var origin: String? = null
     var destination: String? = null
     var latlngUser: LatLng? = null
+    private var greyPolyLineList = arrayListOf<Polyline>()
+    private var orangePolyLineList = arrayListOf<Polyline>()
+    private var originMarker: Marker? = null
+    private var destinationMarker: Marker? = null
 
     override fun initResourceLayout(): Int {
         return R.layout.activity_notification_doorto_door_maps
@@ -174,24 +180,27 @@ class NotificationDoortoDoorMapsActivity :
 
         //latlng foodtruck
         val latLng = LatLng(dataItem?.latitude!!, dataItem.longitude!!)
-        marker?.remove()
-        marker = googleMapFoodTruck!!.addMarker(
-            MarkerOptions().flat(true).position(latLng).icon(
-                BitmapDescriptorFactory.fromResource(R.drawable.icon_pin_foodtruck)
-            ).title(dataItemNotificationBlast?.name)
-        )
-        googleMapFoodTruck!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14f))
-        marker?.position = latLng
-        animateMarker(marker!!, latLng, false)
+//        val latLngDest = LatLng(dataItemGetStatusCall?.latitudeEndUser!!, dataItemGetStatusCall?.longitudeEndUser!!)
+//        marker?.remove()
+
+        showPath(latLng,latlngUser)
+//        marker = googleMapFoodTruck!!.addMarker(
+//            MarkerOptions().flat(true).position(latLng).icon(
+//                BitmapDescriptorFactory.fromResource(R.drawable.icon_pin_foodtruck)
+//            ).title(dataItemNotificationBlast?.name)
+//        )
+//        googleMapFoodTruck!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 30f))
+//        marker?.position = latLng
+//        animateMarker(marker!!, latLng, false)
 
 
         origin = "${dataItem.latitude},${dataItem.longitude}"
 
-        googleMapFoodTruck!!.addMarker(
-            MarkerOptions().flat(true).position(latlngUser).icon(
-                BitmapDescriptorFactory.fromResource(R.drawable.icon_pin_customer)
-            ).title("Lokasi User")
-        )
+//        googleMapFoodTruck!!.addMarker(
+//            MarkerOptions().flat(true).position(latlngUser).icon(
+//                BitmapDescriptorFactory.fromResource(R.drawable.icon_pin_customer)
+//            ).title("Lokasi User")
+//        )
 
         if (dataItemGetStatusCall != null) {
             destination =
@@ -211,6 +220,72 @@ class NotificationDoortoDoorMapsActivity :
 //                BitmapDescriptorFactory.fromResource(R.drawable.icon_pin_customer)
 //            ).title("Lokasi Anda")
 //        )
+    }
+
+    private fun moveCamera(latLng: LatLng?) {
+        googleMap?.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+    }
+
+    private fun showPath(latLngData: LatLng?,latLngData1: LatLng?) {
+        val latLngList = arrayListOf<LatLng>()
+        val builder = LatLngBounds.Builder()
+//        for (latLngData in latLngList.latLngList) {
+            val latLng = LatLng(latLngData!!.latitude, latLngData.longitude)
+          val latLngDest = LatLng(latLngData1!!.latitude, latLngData1.longitude)
+            builder.include(latLng)
+            latLngList.add(latLng)
+        latLngList.add(latLngDest)
+//        }
+        val bounds = builder.build()
+
+        moveCamera(latLngList[0])
+        googleMapFoodTruck?.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 2))
+        val polylineOptions = PolylineOptions()
+        polylineOptions.color(Color.GRAY)
+        polylineOptions.width(5f)
+        polylineOptions.addAll(latLngList)
+        val greyPolyLine = googleMapFoodTruck!!.addPolyline(polylineOptions)
+        greyPolyLineList.add(greyPolyLine)
+
+        val blackPolylineOptions = PolylineOptions()
+        blackPolylineOptions.width(5f)
+        blackPolylineOptions.color(Color.parseColor("#EF6623"))
+        val orangePolyLine = googleMapFoodTruck!!.addPolyline(blackPolylineOptions)
+        orangePolyLineList.add(orangePolyLine)
+
+        originMarker = addOriginPickupMarkerAndGet(latLngList[0])
+        originMarker!!.setAnchor(0.5f, 0.5f)
+
+        destinationMarker = addOriginDestinationMarkerAndGet(latLngList[latLngList.size - 1])
+        destinationMarker?.setAnchor(0.5f, 0.5f)
+
+        val polylineAnimator = AnimationUtils.polyLineAnimator()
+        polylineAnimator.addUpdateListener { valueAnimator ->
+            val percentValue = (valueAnimator.animatedValue as Int)
+            val index = (greyPolyLine.points!!.size * (percentValue / 100.0f)).toInt()
+            orangePolyLine.points = greyPolyLine.points!!.subList(0, index)
+        }
+        polylineAnimator.start()
+    }
+
+    private fun addOriginPickupMarkerAndGet(latLng: LatLng): Marker {
+        val bitmapDescriptor =
+            BitmapDescriptorFactory.fromBitmap(com.zeepos.map.utils.MapUtils.getPickupDestinationBitmap())
+        return googleMap!!.addMarker(
+            MarkerOptions().flat(true).position(latLng).icon(
+                BitmapDescriptorFactory.fromResource(R.drawable.icon_pin_foodtruck)
+            ).title(dataItemNotificationBlast?.name))
+
+    }
+
+    private fun addOriginDestinationMarkerAndGet(latLng: LatLng): Marker {
+        val bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(com.zeepos.map.utils.MapUtils.getDestinationBitmap())
+        return googleMap!!.addMarker(
+            MarkerOptions().flat(true).position(latlngUser).icon(
+                BitmapDescriptorFactory.fromResource(R.drawable.icon_pin_customer)
+            ).title("Lokasi User")
+        )
+
     }
 
     fun animateMarker(
@@ -258,7 +333,7 @@ class NotificationDoortoDoorMapsActivity :
             ?.requestDirectionMaps(
                 origin,
                 destination,
-                application.getString(R.string.google_maps_key)
+                application.getString(R.string.google_api_key)
             )
             ?.enqueue(object : Callback<ResponseDirectionsMaps?> {
                 override fun onResponse(
@@ -269,21 +344,21 @@ class NotificationDoortoDoorMapsActivity :
                     val responseDirectionsMaps = response.body()
                     if (responseDirectionsMaps?.routes?.isNotEmpty()!!) {
                         val list =
-                            MapUtils.decodePoly(responseDirectionsMaps.routes?.get(0)?.overviewPolyline?.points)
+                            MapUtils.decodePoly(responseDirectionsMaps!!.routes?.get(0)?.overviewPolyline?.points)
                         val getDuration =
-                            responseDirectionsMaps.routes?.get(0)?.legs?.get(0)?.duration?.text
+                            responseDirectionsMaps!!.routes?.get(0)?.legs?.get(0)?.duration?.text
                         val getKm =
                             responseDirectionsMaps.routes?.get(0)?.legs?.get(0)?.distance?.text
 
 
-                        if (dataItemGetStatusCall?.status.equals("EXPIRE")) {
+                        if (dataItemGetStatusCall?.status.equals("EXPIRE")!!) {
                             hideView(tvDurationEstimate)
                         } else showView(tvDurationEstimate)
 
                         tvDurationEstimate.text =
                             "Jarak tempuh Anda $getKm dengan estimasi sampai $getDuration"
 
-//                        addPolyline(list, destinationLatlng)
+//                        addPolyline(list)
                     } else {
                         hideView(tvDurationEstimate)
                     }
@@ -295,7 +370,7 @@ class NotificationDoortoDoorMapsActivity :
             })
     }
 
-    fun addPolyline(list: MutableList<LatLng>, destination: LatLng) {
+    fun addPolyline(list: MutableList<LatLng>) {
         for (z in 0 until list.size - 1) {
             val src: LatLng = list[z]
             val dest: LatLng = list[z + 1]
